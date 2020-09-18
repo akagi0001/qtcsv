a1a6ea45594105e34d4626a76e1ca537c5ed9598
telecomdev89 <willsonsami.ws@gmail.com>
2020-09-18 09:32:15 +0200

    Fix qrand() and qsrand() Qt 5.15 deprecation

--- src/sources/writer.cpp
+++ src/sources/writer.cpp
@@ -7,6 +7,9 @@
 #include <QTextStream>
 #include <QCoreApplication>
 #include <QDebug>
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
+#include <QRandomGenerator>
+#endif
 
 #include "include/abstractdata.h"
 #include "filechecker.h"
@@ -146,7 +149,13 @@ QString WriterPrivate::getTempFileName()
 
     for (int counter = 0; counter < std::numeric_limits<int>::max(); ++counter)
     {
-        QString name = nameTemplate.arg(QString::number(qrand()));
+#if QT_VERSION < QT_VERSION_CHECK(5, 10, 0)
+        const quint32 number = qrand();
+#else
+        const quint32 number = QRandomGenerator::global()->generate();
+#endif
+
+        QString name = nameTemplate.arg(QString::number(number));
         if ( false == QFile::exists(name) )
         {
             return name;
